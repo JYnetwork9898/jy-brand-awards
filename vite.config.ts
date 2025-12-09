0e5fe8cd9cc8643f92e3fed28122fbbd723aa5cb
ele8385 <60867752+ele8385@users.noreply.github.com>
2025-12-09 09:57:11 +0900

    seo최적화

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { CURRENT_YEAR } from './src/constants/common'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        return html.replace(/%CURRENT_YEAR%/g, CURRENT_YEAR.toString())
+      }
+    }
+  ],
 })
