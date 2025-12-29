2138cedb4c4ca739b49f5d52af5acc76333cbee7
ele8385 <60867752+ele8385@users.noreply.github.com>
2025-12-29 10:44:03 +0900

    awards/링크 제외해도 접속되게

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,12 @@ function App() {
         {/* 기본 경로 접속 시 히트브랜드 페이지로 리다이렉트(임시 설정) */}
         <Route path="/" element={<Navigate to="/awards/THBA" replace />} />
 
+        {/* Short URL redirects */}
+        <Route path="/thba" element={<Navigate to="/awards/THBA" replace />} />
+        <Route path="/cseb" element={<Navigate to="/awards/CSEB" replace />} />
+        <Route path="/csba" element={<Navigate to="/awards/CSBA" replace />} />
+        <Route path="/teba" element={<Navigate to="/awards/TEBA" replace />} />
+
         {/* 메인 랜딩 페이지 */}
         <Route path="/home" element={<Home />} />
 
